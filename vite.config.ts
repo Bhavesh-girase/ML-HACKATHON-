63bc43d4309bd7685944be237221fa7fafb23578
Bhavesh-girase <bhaveshgirase27@gmail.com>
2025-08-08 21:12:15 +0530

    refresh try

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,12 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base:"/",
+  build:{outDir:"dist"},
   server: {
     host: "::",
     port: 8080,
+    historyApiFallback: true
   },
   plugins: [
     react(),
